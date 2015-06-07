11961e76ad44c3110d98dc418fd6f9c5557114d2
Menno <mennomoes@hotmail.com>
2015-06-07 19:42:18 +0200

    Chat

---
+++ Chat/xamarin SignalRChat/Droid/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Views.InputMethods;
+using Android.Widget;
+using SignalRChat;
+using Android.Content.PM;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRChat.Droid
+{
+	[Activity (Label = "SignalRChat.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : Activity
+	{
+		protected override async void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			SetContentView(Resource.Layout.Main);
+
+			var messageListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, new List<string>());
+			var messageList = FindViewById<ListView>(Resource.Id.Messages);
+			messageList.Adapter = messageListAdapter;
+
+			var hubConnection = new HubConnection ("http://192.168.1.42:8080");
+			var hubProxy = hubConnection.CreateHubProxy ("MyHub");
+			hubProxy.On<string>("UpdateChatMessage", message2 => 
+				messageList.Text += string.Format("Received Msg: {0}\r\n", message2));
+			//var client = new Client("Android");
+
+			var sendMessage = FindViewById<Button>(Resource.Id.SendMessage);
+			var message = FindViewById<EditText>(Resource.Id.Input);
+
+			sendMessage.Click += delegate
+			{
+				if (!string.IsNullOrWhiteSpace(message.Text))
+				{
+					hubConnection.Send("Android: " + message.Text);
+
+					RunOnUiThread(() => message.Text = "");
+				}
+			};
+				
+			await hubConnection.Start ().ContinueWith (task => hubConnection.Send ("Android: connected"));;
+		}
+	}
+}
+
---
+++ Chat/xamarin SignalRChat/SignalRChat/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRChat
+{
+	public class Client
+	{
+		private readonly string _platform;
+		private readonly HubConnection _connection;
+		private readonly IHubProxy _proxy;
+
+		//public event EventHandler<string> OnMessageReceived;
+
+		public Client(string platform)
+		{
+			_platform = platform;
+			_connection = new HubConnection("http://localhost:8080");
+			_proxy = _connection.CreateHubProxy("hubs");
+
+			Connect ();
+		}
+
+		public async Task Connect()
+		{
+			await _connection.Start();
+
+			/*_proxy.On("messageReceived", (string platform, string message) =>
+				{
+					if (OnMessageReceived != null)
+						OnMessageReceived(this, string.Format("{0}: {1}", platform, message));
+				});
+
+			Send("Connected");*/
+		}
+
+		public Task Send(string message)
+		{
+			return _proxy.Invoke("Send", _platform, message);
+		}
+	}
+}
+
